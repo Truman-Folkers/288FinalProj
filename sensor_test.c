bf5c1d69d77c914535c33c03478e35c19532c3db
ppetsche <ppetsche@iastate.edu>
2026-04-26 16:47:10 -0500

    added test options for hole

---
+++ sensor_test.c
@@ -0,0 +1,28 @@
+/**
+ * 
+ * sensor_test.c
+ * 
+ * Tests sensors for hole and tape
+ */
+
+#include "timer.h"
+#include "lcd.h"
+#include "open_interface.h"
+#include "sensor_test.h"
+#include <stdint.h>
+#include <inc/tm4c123gh6pm.h>
+
+ void test_hole(oi_t *sensor_data){
+
+
+   //drives forward into hole. When cliff senses the hole, it backs up
+   //purpose: test the front cliff sensors
+    oi_setWheels(100,100);
+    while(!(sensor_data.cliffFrontLeft | sensor_data.cliffFrontRight)){};
+    oi_setWheels(-100,-100);
+
+    //test light bump sensors
+    //supposed to detect walls and low objects, like the hole
+    //packet id 45-51
+
+ }
---
+++ sensor_test.h
@@ -0,0 +1,19 @@
+/**
+ * 
+ * sensor_test.h
+ */
+
+
+ #ifndef SENSOR_TEST_H_
+#define SENSOR_TEST_H_
+
+
+#include "Timer.h"
+#include "lcd.h"
+#include "open_interface.h"
+#include <stdint.h>
+#include <inc/tm4c123gh6pm.h>
+
+void test_hole(oi_t *sensor_data);
+
+#endif /* SENSOR_TEST_H_ */
