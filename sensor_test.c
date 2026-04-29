a29e51eaf4e2fd241b9e3fd78cb13034d63aa1e5
Petsche <ppetsche@iastate.edu>
2026-04-29 18:14:03 -0500

    Added calibration and hole sensors

--- sensor_test.c
+++ sensor_test.c
@@ -26,3 +26,40 @@
 //    //packet id 45-51
 //
 // }
+void test_hole(oi_t *sensor_data){
+
+
+  //drives forward into hole. When cliff senses the hole, it backs up
+  //purpose: test the front cliff sensors
+   oi_setWheels(100,100);
+   while(!(sensor_data.cliffFrontLeft | sensor_data.cliffFrontRight)){};
+   oi_setWheels(-100,-100);
+
+   //test light bump sensors
+   //supposed to detect walls and low objects, like the hole
+   //packet id 45-51
+
+   while(1){
+    oi_update(sensor_data);
+   }
+   if(lightBumpLeftSignal > 3500){
+    sendString("COL:%i, %i", 150, lightBumpLeftSignal);
+   }
+   if(lightBumpFrontLeftSignal > 3500){
+    sendString("COL:%i, %i", 125, lightBumpFrontLeftSignal);
+   }
+   if(lightBumpCenterLeftSignal > 3500){
+    sendString("COL:%i, %i", 100, lightBumpCenterLeftSignal);
+   }
+   if(lightBumpCenterRightSignal > 3500){
+    sendString("COL:%i, %i", 80, lightBumpCenterRightSignal);
+   }
+   if(lightBumpFrontRightSignal > 3500){
+    sendString("COL:%i, %i", 55, lightBumpFrontRightSignal);
+   }
+   if(lightBumpRightSignal > 3500){
+    sendString("COL:%i, %i", 30, lightBumpRightSignal);
+   }
+   
+
+}
